3a2bd780b7bc4ca669ed13e52d6119420ea3634b
wicky <wicky@qq.com>
2026-04-27 15:51:27 +0800

    init

---
+++ Hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// zhsehi djakdfklj
+func main() {
+	fmt.Println("Hello World6666664545466666")
+	maind()
+}
+
+func maind() {
+
+	name := "John"
+	address := "shenz"
+	fmt.Println(name, address)
+
+}
